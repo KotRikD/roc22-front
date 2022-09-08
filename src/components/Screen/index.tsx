1a3ea3961e84c415f24fc97a724685bb65602202
Mikhail Babynichev <i@kotrik.ru>
2022-09-08 23:55:59 +0300

    add: new video

--- src/components/Screen/index.tsx
+++ src/components/Screen/index.tsx
@@ -1,7 +1,24 @@
 import React, { PropsWithChildren } from 'react';
 
+import rocBG from '@/assets/rocbackground.webm';
+
 import styles from './index.module.scss';
 
 export const Screen: React.FC<PropsWithChildren> = (props) => {
-	return <div className={styles.Screen}>{props.children}</div>;
+	return (
+		<div className={styles.Screen}>
+			{props.children}
+			<video
+				src={rocBG}
+				autoPlay
+				loop
+				muted
+				style={{
+					position: 'absolute',
+					top: 0,
+					zIndex: -1
+				}}
+			/>
+		</div>
+	);
 };
