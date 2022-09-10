fb3f7a4cce7f2684d2f6f65bb953fc4149fc927a
Mikhail Babynichev <i@kotrik.ru>
2022-09-10 16:39:53 +0300

    fix: forget to add file

---
+++ src/utils/usePreviousNonNull.ts
@@ -0,0 +1,11 @@
+import { useEffect, useRef } from "react";
+
+export const usePreviousNonNull = <T extends unknown>(value: T): T | undefined => {
+    const ref = useRef<T>();
+    useEffect(() => {
+      if (!value) return;
+  
+      ref.current = value;
+    });
+    return ref.current;
+  };
